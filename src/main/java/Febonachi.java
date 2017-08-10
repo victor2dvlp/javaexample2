cb1efffaef78d92d60ed039f0878d493423dd7ed
victor2dvlp <victor_sp@ukr.net>
2017-08-10 09:26:45 +0500

    updated project structure

---
+++ src/main/java/Febonachi.java
@@ -0,0 +1,22 @@
+/**
+ * Created by VicTOR on 05.08.2017.
+ */
+public class Febonachi {
+    public static void main(String[] args) {
+        int input = 12;
+        int k = 0;
+        int[] prev = {1, 1};
+        //System.out.print("begin: " + prev[0] + " " + prev[1] + "\n");
+        for(int i = 0; i < input; i++){
+            if( i < 2 )
+            System.out.print(prev[i] + " ");
+            else{
+                k = prev[0] + prev[1];
+                prev[0] = prev[1];
+                prev[1] = k;
+                System.out.print(k + " ");
+            }
+            //comment
+        }
+    }
+}
